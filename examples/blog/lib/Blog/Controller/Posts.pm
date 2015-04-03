2235d190513fd6ab1555e43c75b43a08d9f9d81f
Svetoslav Naydenov <harryl@cpan.org>
2015-04-03 14:44:06 +0300

    updated blog example from Mojo::Pg 2.03

--- examples/blog/lib/Blog/Controller/Posts.pm
+++ examples/blog/lib/Blog/Controller/Posts.pm
@@ -1,9 +1,21 @@
 package Blog::Controller::Posts;
 use Mojo::Base 'Mojolicious::Controller';
 
+sub create { shift->stash(post => {}) }
+
+sub edit {
+  my $self = shift;
+  $self->stash(post => $self->posts->find($self->param('id')));
+}
+
+sub index {
+  my $self = shift;
+  $self->stash(posts => $self->posts->all);
+}
+
 sub remove {
   my $self = shift;
-  $self->posts->withdraw($self->param('id'));
+  $self->posts->remove($self->param('id'));
   $self->redirect_to('posts');
 }
 
@@ -15,15 +27,33 @@ sub show {
 sub store {
   my $self = shift;
 
+  my $validation = $self->_validation;
+  return $self->render(action => 'create', post => {})
+    if $validation->has_error;
+
+  my $id = $self->posts->add($validation->output);
+  $self->redirect_to('show_post', id => $id);
+}
+
+sub update {
+  my $self = shift;
+
+  my $validation = $self->_validation;
+  return $self->render(action => 'edit', post => {}) if $validation->has_error;
+
+  my $id = $self->param('id');
+  $self->posts->save($id, $validation->output);
+  $self->redirect_to('show_post', id => $id);
+}
+
+sub _validation {
+  my $self = shift;
+
   my $validation = $self->validation;
   $validation->required('title');
   $validation->required('body');
-  return $self->render('posts/create') if $validation->has_error;
 
-  my $title = $validation->param('title');
-  my $body  = $validation->param('body');
-  my $id    = $self->posts->publish($title, $body);
-  $self->redirect_to('show_post', id => $id);
+  return $validation;
 }
 
 1;
--- examples/blog/lib/Blog/Model/Posts.pm
+++ examples/blog/lib/Blog/Model/Posts.pm
@@ -3,19 +3,25 @@ use Mojo::Base -base;
 
 has 'mysql';
 
-sub all { shift->mysql->db->query('select * from posts')->hashes->each }
+sub add {
+  my ($self, $post) = @_;
+  my $sql = 'insert into posts (title, body) values (?, ?)';
+  return $self->mysql->db->query($sql, $post->{title}, $post->{body})->last_insert_id;
+}
+
+sub all { shift->mysql->db->query('select * from posts')->hashes->to_array }
 
 sub find {
   my ($self, $id) = @_;
   return $self->mysql->db->query('select * from posts where id = ?', $id)->hash;
 }
 
-sub publish {
-  my ($self, $title, $body) = @_;
-  my $sql = 'insert into posts (title, body) values (?, ?)';
-  return $self->mysql->db->query($sql, $title, $body)->last_insert_id;
-}
+sub remove { shift->mysql->db->query('delete from posts where id = ?', shift) }
 
-sub withdraw { shift->mysql->db->query('delete from posts where id = ?', shift) }
+sub save {
+  my ($self, $id, $post) = @_;
+  my $sql = 'update posts set title = ?, body = ? where id = ?';
+  $self->mysql->db->query($sql, $post->{title}, $post->{body}, $id);
+}
 
 1;
