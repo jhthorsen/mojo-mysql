41a3685b8d5b512f30a59dae326e2a8b9b951e30
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-05-15 20:31:39 +0200

    Prevent warnings when creating the mojo_migrations table, closes #26

--- lib/Mojo/mysql/Migrations.pm
+++ lib/Mojo/mysql/Migrations.pm
@@ -144,7 +144,7 @@ sub _active {
 
   $db->query(
     'create table if not exists mojo_migrations (
-       name    varchar(255) unique not null,
+       name    varchar(128) unique not null,
        version bigint not null
      )'
   ) if $error;
