1cb0887a48fb2faf9521e986419bcb4387993354
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-02-27 19:32:53 +0100

    Released version 1.06
     - Changed from_string() to also accept Mojo::URL objects #36
       Contributor: Karl Rune Nilsen

--- lib/Mojo/mysql.pm
+++ lib/Mojo/mysql.pm
@@ -9,7 +9,7 @@ use Mojo::URL;
 use Scalar::Util 'weaken';
 use SQL::Abstract;
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 has abstract => sub { SQL::Abstract->new(quote_char => chr(96), name_sep => '.') };
 has auto_migrate    => 0;
